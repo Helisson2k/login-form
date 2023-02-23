380af2f43fa87c06ce6db89b4fd961536d13b5b4
Helisson2k <122854897+Helisson2k@users.noreply.github.com>
2023-02-23 04:23:57 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let container = document.querySelector("inputbox");
-let input = document.querySelector("input");
-let icon = document.querySelector("img");
-
-icon.addEventListener('click',function(){
-    container.classList.toggle('visible')
-    if(container.classList.container('visible'))
-    icon.
-})
